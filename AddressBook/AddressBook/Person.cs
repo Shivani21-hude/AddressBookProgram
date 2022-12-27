16c0f8001e06ec69bd0dab54c30baf4b478a7eba
Shivani21-hude <shivani21hude@gmail.com>
2022-12-27 17:24:13 +0530

    [Shivani]-Adding Contacts

---
+++ AddressBook/AddressBook/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class Person
+    {
+        internal static void CreateContacts()
+        {
+            Contact contact = new Contact();
+            Console.WriteLine("Enter First Name :");
+            contact.firstname = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name :");
+            contact.lastname = Console.ReadLine();
+
+            Console.WriteLine("Enter Address :");
+            contact.address = Console.ReadLine();
+
+            Console.WriteLine("Enter city :");
+            contact.city = Console.ReadLine();
+
+            Console.WriteLine("Enter state :");
+            contact.state = Console.ReadLine();
+
+            Console.WriteLine("Enter zipcode :");
+            contact.zipcode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Mobile Number :");
+            contact.phonenumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter email :");
+            contact.email = Console.ReadLine();
+        }
+
+    }
+}
---
+++ AddressBook/AddressBook/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addressbook
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("      'Welcome to Address Book program'");
+            Console.WriteLine("\n");
+            AddressBook.Person.CreateContacts();
+        }
+    }
+}
